80498630e1edff984b3af7a13b7f55d3f66037d9
Joshua Causey <joshpcausey@gmail.com>
2018-01-17 13:32:21 -0600

    Adding FizzBuzz solution

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.ReadLine();
+        }
+
+        static void Execute()
+        {
+            //TODO:  Implement FizzBuzz
+            int finalNumber = 100;
+            for(int i = 1; i <= finalNumber; i++)
+            {
+                if(i%3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz");
+                }
+                else if(i%5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz");
+                }
+                else if(i%3 == 0)
+                {
+                    Console.WriteLine(i + " Fiz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
