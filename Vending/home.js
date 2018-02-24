f216e1bb3bfe0c5c40ade8440621c1aadbaa579e
Joshua Causey <joshpcausey@gmail.com>
2018-02-24 17:11:37 -0600

    Added total $ in section and messages section

--- Vending/home.js
+++ Vending/home.js
@@ -1,32 +1,37 @@
 //ts-check
-$(document).ready(function () {
-    loadItems();
+$(document).ready(function() {
+  loadItems();
 });
 
 function loadItems() {
-    var contentRows = $('#contentRows');
+  var contentRows = $('#contentRows');
 
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/items',
-        success: function (itemArray) {
-            $.each(itemArray, function (index, item) {
-                id = item.id;
-                name = item.name;
-                price = item.price;
-                quantity = item.quantity;
+  $.ajax({
+    type: 'GET',
+    url: 'http://localhost:8080/items',
+    success: function(itemArray) {
+      $.each(itemArray, function(index, item) {
+        id = item.id;
+        name = item.name;
+        price = item.price;
+        quantity = item.quantity;
 
-                var row = '<div class="col"><div class="card" style="width: 18rem;"><div class="card-body">';
-                row += '<p>' + id + '</p>';
-                row += '<h5 class="card-title text-center">' + name + '</h5>';
-                row += '<p class="card-text text-center">' + price + '</p>';
-                row += '<p class="text-center">Quantity Left: ' + quantity + '</p>';
-                row += '</div></div></div>';
-                contentRows.append(row);
-            })
-        },
-        error: function () {
-
-        }
-    });
+        var row = '<div class="col"><div class="card" style="width: 18rem;"><div class="card-body">';
+        row += '<p id="' + id + '"">' + id + '</p>';
+        row += '<h5 class="card-title text-center">' + name + '</h5>';
+        row += '<p class="card-text text-center">$' + price + '</p>';
+        row += '<p class="text-center">Quantity Left: ' + quantity + '</p>';
+        row += '</div></div></div>';
+        contentRows.append(row);
+      });
+    },
+    error: function() {
+      $('#errorMessage')
+        .append($('<li>')
+          .attr({
+            class: 'list-group-item list-group-item-danger'
+          })
+          .text('Error calling web service, please try again later'));
+    }
+  });
 }
